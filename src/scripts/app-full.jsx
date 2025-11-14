a7e254ac7270c023f28208c5024663d0e4a3b284
Brenda Calazans Guimarães <62815330+brendacalazans@users.noreply.github.com>
2025-11-14 20:50:40 -0300

    Update app-full.jsx

--- src/scripts/app-full.jsx
+++ src/scripts/app-full.jsx
@@ -2521,7 +2521,7 @@
                                 className="w-auto bg-white/10 hover:bg-white/20 text-white font-mono px-4 py-2 rounded-lg transition-colors disabled:opacity-50"
                             >
                                 {part}
-
+                            </button>
                         ))}
                     </div>
                     
